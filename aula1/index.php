beebe5896f42dd8290d8350dadd8c30006d65846
Ráfaga Matos de Souza <92107189+rafagamatos@users.noreply.github.com>
2022-07-13 16:50:35 -0300

    Aula 1

---
+++ aula1/index.php
@@ -0,0 +1,23 @@
+<?php 
+//Atividade 1  
+   /* $i = 1;
+    $j = 1;
+    for ($i=0; $i < 10; $i++){
+        echo "<br/>";
+        for ($j=0; $j < 10; $j++){
+            echo " - ";
+        }
+    }*/
+
+    //atividade 2 
+    $i = 1;
+    $j = 1;
+    $elem = " - ";
+    for ($i=0; $i < 20; $i++){
+        //echo "<br>";
+        echo str_repeat($elem, $i)."<br/>";
+        
+    }
+
+   
+?>
